2b088ae784e673ad7a0047839a357192d9194b26
Ryan Kepler <ryankepler@Ryans-MacBook-Pro.local>
2014-11-22 21:29:57 -0500

    this is the first script that works making the hash reference properly

---
+++ scripts/parseGBnum/parseGBnum.pl
@@ -0,0 +1,25 @@
+#!usr/bin/perl
+use strict;
+use warnings;
+
+#This script will read in a tab-delimited file returned directly from genbank and 
+#parse the data into a table, with a separate line for each sample and each
+#accession in a column of its own. 
+
+#usage:parseGBnum.pl INFILE OUTFILE 
+
+my $list = shift @ARGV;
+my $outfile = shift @ARGV;
+
+open (IN, '<', $list);
+my %gb_hash;
+ 
+while (my $line = <IN>) {
+	chomp $line;
+	my ($gene, $sample, $gbnum) = split(/\s+/, $line);
+	$gene =~ s/\..+//;
+	$gb_hash{$sample}{$gene} = $gbnum;
+}
+my $thing;
+my $placeholder:
+__END__
